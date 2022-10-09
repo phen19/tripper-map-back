4221b2ba92f149c61f68141c6f46e90d3a2ba36b
ph-ubuntu-pc <phenriquelgsilva@gmail.com>
2022-10-09 10:51:27 -0300

    feat: error logging

--- src/middlewares/errorHandler.ts
+++ src/middlewares/errorHandler.ts
@@ -1,18 +1,23 @@
-import {Request, Response, NextFunction} from "express";
+import { Request, Response, NextFunction } from "express";
 
-export default function errorHandler (error:any, req: Request, res: Response, next: NextFunction) {
-  if(error.code === 'NotFound') {
-    return res.status(404).send(error.message)
+export default function errorHandler(
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (error.code === "NotFound") {
+    return res.status(404).send(error.message);
   }
-  if(error.code === 'UnprocessableEntry') {
-    return res.status(422).send(error.message)
+  if (error.code === "UnprocessableEntry") {
+    return res.status(422).send(error.message);
   }
-  if(error.code === 'Conflict') {
-    return res.status(409).send(error.message)
+  if (error.code === "Conflict") {
+    return res.status(409).send(error.message);
   }
-  if(error.code === 'Unauthorized'){
-    return res.status(401).send(error.message)
+  if (error.code === "Unauthorized") {
+    return res.status(401).send(error.message);
   }
-
+  console.log(error);
   res.sendStatus(500);
 }
